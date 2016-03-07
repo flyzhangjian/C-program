e87f4808b3f20fc9b9705a641a7e678336297d50
zhangjian <1836810995@qq.com>
2016-03-07 14:29:02 +0800

    initial

---
+++ lianxi.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: lianxi.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年03月07日 星期一 14时01分21秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+    char name[20];
+    printf("please input your name:\n");
+    scanf("%s",name);
+    printf("%d",name[0]);
+
+    return 0;
+}
